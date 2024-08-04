ded80a79b3ae684ab7bb5abd80ee7bcf1f7e7dfd
bexposito <>
2024-08-04 17:52:27 +0200

    ImportController/ImportService - added JSON importation

--- src/main/java/org/acme/ImportController.java
+++ src/main/java/org/acme/ImportController.java
@@ -4,68 +4,49 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.acme.model.rest.ColumnHeaderRest;
 import org.acme.model.rest.GridRest;
+import org.acme.service.GridService;
 import org.acme.service.ImportService;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Path("/")
+@Path("/import")
 public class ImportController {
 
     @Inject
     ImportService importService;
 
+    @Inject
+    GridService gridService;
+
     @GET
     @Path("/getExampleData")
     @Produces(MediaType.APPLICATION_JSON)
     public GridRest getExampleData() {
-        List<Map<String, Object>> cars = new ArrayList<>();
-
-        Map<String, Object> car1 = new HashMap<>();
-        car1.put("make", "Toyota");
-        car1.put("model", "Celica");
-        car1.put("price", 56655);
-
-        Map<String, Object> car2 = new HashMap<>();
-        car2.put("make", "Ford");
-        car2.put("model", "Mondeo");
-        car2.put("price", 40000);
-
-        Map<String, Object> car3 = new HashMap<>();
-        car3.put("make", "Porsche");
-        car3.put("model", "Boxster");
-        car3.put("price", 900000);
-
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
-
-        List<ColumnHeaderRest> lst = new ArrayList<>();
-        lst.add(new ColumnHeaderRest("Make", "make", true));
-        lst.add(new ColumnHeaderRest("Model", "model", true));
-        lst.add(new ColumnHeaderRest("Price", "price", true));
-
-        GridRest rs = new GridRest();
-        rs.setValues(cars);
-        rs.setHeader(lst);
-
-        return rs;
+        return gridService.getGrid();
     }
 
     @POST
-    @Path("/upload")
+    @Path("/csv")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response uploadFile(MultipartFormDataInput input) {
-        GridRest rs = importService.multipartToGrid(input);
+    public Response importCsv(MultipartFormDataInput input) {
+        GridRest rs = importService.multipartCsvToGrid(input);
         if(rs == null) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error processing file").build();
         }
+        this.gridService.setGrid(rs);
         return Response.ok(rs).build();
     }
+
+
+    @POST
+    @Path("/json")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response importJson(MultipartFormDataInput input) {
+        GridRest rs = importService.multipartJsonToGrid(input);
+        this.gridService.setGrid(rs);
+        return Response.ok(this.gridService.getGrid()).build();
+    }
+
 }
--- src/main/java/org/acme/service/ImportService.java
+++ src/main/java/org/acme/service/ImportService.java
@@ -1,58 +1,98 @@
 package org.acme.service;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.core.Response;
 import org.acme.model.rest.ColumnHeaderRest;
 import org.acme.model.rest.GridRest;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class ImportService {
-	public GridRest multipartToGrid(MultipartFormDataInput input) {
-		try {
-			InputPart inputPart = input.getFormDataMap().get("file").get(0);
-			String fileName = inputPart.getHeaders().getFirst("Content-Disposition");
-			GridRest rs = new GridRest();
 
-			if (fileName != null) {
-				String[] contentDisposition = fileName.split(";");
-				for (String cd : contentDisposition) {
-					if (cd.trim().startsWith("filename")) {
-
-						//Reads the file
-						InputStream inputStream = inputPart.getBody(InputStream.class, null);
-						Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-
-						CSVParser parser = new CSVParser(reader, CSVFormat.EXCEL);
-						List<CSVRecord> list = parser.getRecords();
-
-						for (int line = 0; line < list.size(); line++) {
-							for (int j = 0; j < list.get(line).size(); j++) {
-								if(line == 0) {
-									rs.addHeader(new ColumnHeaderRest("Column " + j, "column_" + j, true));
-								}
-								rs.addValue(line, "column_"+j, list.get(line).get(j));
-							}
-						}
-
-					}
-				}
-
-				return rs;
+	public String multipartToString(MultipartFormDataInput input){
+		StringBuilder stringBuilder = new StringBuilder();
+		input.getFormDataMap().forEach((key, value) -> {
+			try {
+				stringBuilder.append(value.get(0).getBodyAsString());
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
+		});
+		return stringBuilder.toString();
+	}
+
+	public GridRest multipartCsvToGrid(MultipartFormDataInput input) {
+		String csv = multipartToString(input);
+		InputStream inputStream = new ByteArrayInputStream(csv.getBytes(StandardCharsets.UTF_8));
+		Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+
+		CSVParser parser = null;
+		GridRest rs = new GridRest();
+		try {
+			parser = new CSVParser(reader, CSVFormat.EXCEL);
+			List<CSVRecord> list = parser.getRecords();
+			for (int line = 0; line < list.size(); line++) {
+				for (int j = 0; j < list.get(line).size(); j++) {
+					if(line == 0) {
+						rs.addHeader(new ColumnHeaderRest("Column " + j, "column_" + j, true));
+					}
+					rs.addValue(line, "column_"+j, list.get(line).get(j));
+				}
+			}
+		} catch (IOException e) {
 			return null;
 		}
-		return null;
+		return rs;
+	}
+
+	public GridRest multipartJsonToGrid(MultipartFormDataInput input) {
+		GridRest rs = new GridRest();
+		String json = multipartToString(input);
+		processJson(JsonParser.parseString(json), rs);
+		return rs;
+	}
+
+
+	private void processJson(JsonElement jsonElement, GridRest rs) {
+		if (jsonElement.isJsonArray()) {
+			JsonArray jsonArray = jsonElement.getAsJsonArray();
+
+			for (int i = 0; i < jsonArray.size(); i++) {
+				if( i == 0){
+					setGridHeadersFromJson((JsonObject) jsonArray.get(i), rs);
+				}
+				setGridValuesFromJson((JsonObject) jsonArray.get(i), i, rs);
+			}
+		} else if (jsonElement.isJsonObject()) {
+			JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+			setGridValuesFromJson(jsonObject, 0,rs);
+			setGridHeadersFromJson(jsonObject, rs);
+		}
+	}
+
+	private void setGridHeadersFromJson(JsonObject jsonObject, GridRest rs ) {
+		for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+			rs.addHeader(new ColumnHeaderRest(entry.getKey(), entry.getKey(),true));
+		}
+	}
+
+	private void setGridValuesFromJson(JsonObject jsonObject, int line, GridRest rs) {
+		for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+			String key = entry.getKey();
+			JsonElement value = entry.getValue();
+			rs.addValue(line, key, value.toString());
+		}
 	}
 }
