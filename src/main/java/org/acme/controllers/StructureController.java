c9149916af9bb26267b11fcbe796bc7052c92f6f
bexposito <>
2024-08-21 07:38:06 +0200

    Structure - Delete row & column

--- src/main/java/org/acme/controllers/StructureController.java
+++ src/main/java/org/acme/controllers/StructureController.java
@@ -5,6 +5,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.acme.model.rest.GridRest;
 import org.acme.service.StructureService;
+import org.acme.util.Utils;
 
 @Path("/structure")
 public class StructureController {
@@ -25,4 +26,20 @@ public class StructureController {
     public GridRest addRow(@FormParam("position") int position) {
         return structureService.addRow(position);
     }
+
+    @POST
+    @Path("/deleteRows")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public GridRest deleteRows(@FormParam("indexes") String indexes) {
+        return structureService.deleteRows(Utils.text2IntArray(indexes));
+    }
+
+
+    @POST
+    @Path("/deleteColumns")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public GridRest deleteColumns(@FormParam("indexes") String indexes) {
+        return structureService.deleteColumns(Utils.text2IntArray(indexes));
+    }
+
 }
--- src/main/java/org/acme/service/StructureService.java
+++ src/main/java/org/acme/service/StructureService.java
@@ -5,7 +5,7 @@ import jakarta.inject.Inject;
 import org.acme.model.rest.ColumnHeaderRest;
 import org.acme.model.rest.GridRest;
 
-import java.util.HashMap;
+import java.util.*;
 
 @ApplicationScoped
 public class StructureService {
@@ -30,5 +30,41 @@ public class StructureService {
 	public GridRest addRow(int position) {
 		gridService.getGrid().getValues().add(position, new HashMap<>());
 		return gridService.getGrid();
+	}
+
+	public GridRest deleteRows(List<Integer> indexes) {
+		GridRest grid = this.gridService.getGrid();
+
+		//From bigger to smaller to avoid problems with changes of indexes
+		indexes.sort(Comparator.reverseOrder());
+		for (int index: indexes) {
+			grid.getValues().remove(index);
+		}
+		return grid;
+	}
+
+	public GridRest deleteColumns(List<Integer> indexes) {
+		GridRest grid = this.gridService.getGrid();
+
+		//From bigger to smaller to avoid problems with changes of indexes
+		indexes.sort(Comparator.reverseOrder());
+
+		//Removes headers
+		for (int index: indexes) {
+			grid.getHeader().remove(index);
+		}
+
+		//Removes cells line by line
+		List<Map<String, Object>> rows = grid.getValues();
+		for (Map<String, Object> row: rows) {
+			int index = 0;
+			for (String key : row.keySet()) {
+				if(indexes.contains(index)) {
+					row.remove(key);
+				}
+				index++;
+			}
+		}
+		return grid;
 	}
 }
